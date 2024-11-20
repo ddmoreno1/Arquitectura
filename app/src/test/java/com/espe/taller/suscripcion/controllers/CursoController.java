84bc9efb38ba65c1e46529d5ec2c95038978d62b
dmrodriguez9 <dmrodriguez9@spe.edu.ec>
2024-11-20 18:14:50 -0500

    Actualizaciones 2

--- app/src/test/java/com/espe/taller/suscripcion/controllers/CursoController.java
+++ app/src/test/java/com/espe/taller/suscripcion/controllers/CursoController.java
@@ -4,6 +4,18 @@
  */
 package com.espe.taller.suscripcion.controllers;
 
+import com.espe.taller.suscripcion.dto.CursoDTO;
+import com.espe.taller.suscripcion.models.Curso;
+import com.espe.taller.suscripcion.services.CursoService;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
